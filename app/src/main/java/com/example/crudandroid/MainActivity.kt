6e0f41ef3c5075b78e023da9fc4c86e1a91aa06a
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2022-11-02 20:06:15 -0300

    ADD inserção de objetos no banco

--- app/src/main/java/com/example/crudandroid/MainActivity.kt
+++ app/src/main/java/com/example/crudandroid/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.crudandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.crudandroid.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -16,6 +19,14 @@ class MainActivity : AppCompatActivity() {
         binding.btnAdd.setOnClickListener {
             val intent = Intent(this, AddActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+           val pessoaList = AppDB(this@MainActivity).getPessoaDao().getAllPessoa()
+           Log.e("teste", "onResume: ${pessoaList.size}")
         }
     }
 }
